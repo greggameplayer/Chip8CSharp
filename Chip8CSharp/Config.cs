1f47e5d45dc3c3612043191fa119c54c3c6ec55a
Unknown <gregoire.hage@gmail.com>
2020-05-12 19:07:26 +0200

    Keyboard & Config Refactor (1/2)
    - Move keyboard functions to Keyboard.cs
    - Move Display size conditions to Config.cs
    - Add keyboard keys to the config
    - Add a config version system to rewrite when it's a deprecated version

--- Chip8CSharp/Config.cs
+++ Chip8CSharp/Config.cs
@@ -1,9 +1,10 @@
 ﻿using System.IO;
 using System;
-using System.Runtime.Serialization.Json;
+using SDL2;
 using Newtonsoft.Json;
-using System.Reflection;
-
+using System.Security.Cryptography;
+using System.Dynamic;
+using System.Collections.Generic;
 
 namespace Chip8CSharp
 {
@@ -21,15 +22,32 @@ namespace Chip8CSharp
 
     class Config
     {
+        public int ConfigVersion = 2;
         public int QueueOffset = 0;
         public int DisplayState = 0;
         public int DisplayResolution = 0;
         public int DisplayWidth = 0;
         public int DisplayHeight = 0;
+        public int one = (int)Keyboard.Keys.one;
+        public int two = (int)Keyboard.Keys.two;
+        public int three = (int)Keyboard.Keys.three;
+        public int four = (int)Keyboard.Keys.four;
+        public int five = (int)Keyboard.Keys.five;
+        public int six = (int)Keyboard.Keys.six;
+        public int seven = (int)Keyboard.Keys.seven;
+        public int eight = (int)Keyboard.Keys.eight;
+        public int nine = (int)Keyboard.Keys.nine;
+        public int A = (int)Keyboard.Keys.A;
+        public int B = (int)Keyboard.Keys.B;
+        public int C = (int)Keyboard.Keys.C;
+        public int D = (int)Keyboard.Keys.D;
+        public int E = (int)Keyboard.Keys.E;
+        public int F = (int)Keyboard.Keys.F;
     }
 
     class ConfigEngine
     {
+        const int CURRENT_CONFIG_VERSION = 2;
         string ConfigPath = AppDomain.CurrentDomain.BaseDirectory + "Config.json";
 
         public void Init(out Config configObj)
@@ -40,20 +58,58 @@ namespace Chip8CSharp
 
             if (File.Exists(ConfigPath))
             {
-                Console.WriteLine("read config file !");
-                var tr = new StreamReader(ConfigPath);
-                JsonResult = tr.ReadToEnd();
-                configObj = JsonConvert.DeserializeObject<Config>(JsonResult);
-                tr.Close();
+                readConfig(ConfigPath, JsonResult, configObj);
+                if (configObj.ConfigVersion < CURRENT_CONFIG_VERSION || !(IsPropertyExist(configObj, "ConfigVersion")))
+                {
+                    File.Delete(ConfigPath);
+                    configObj = new Config();
+                    JsonResult = JsonConvert.SerializeObject(configObj, Formatting.Indented);
+
+                    Console.WriteLine("rewrite default config file !");
+                    writeConfig(ConfigPath, JsonResult);
+                    readConfig(ConfigPath, JsonResult, configObj);
+                }
             }
             else
             {
                 Console.WriteLine("create config file !");
-                using (var tw = new StreamWriter(ConfigPath, true))
-                {
-                    tw.WriteLine(JsonResult.ToString());
-                    tw.Close();
-                }
+                writeConfig(ConfigPath, JsonResult);
+            }
+        }
+
+        public void setWindowSizeDynamically(Config configObj, IntPtr window, SDL.SDL_DisplayMode DM)
+        {
+            if (configObj.DisplayState == (int)DisplayState.Windowed && configObj.DisplayResolution == (int)DisplayResolution.Available)
+                SDL.SDL_SetWindowSize(window, DM.w, DM.h - 70);
+            else if (configObj.DisplayState == (int)DisplayState.Fullscreen && configObj.DisplayResolution == (int)DisplayResolution.Available)
+                SDL.SDL_SetWindowSize(window, DM.w, DM.h);
+            else if (configObj.DisplayResolution != (int)DisplayResolution.Available)
+                SDL.SDL_SetWindowSize(window, configObj.DisplayWidth, configObj.DisplayHeight);
+        }
+
+        public static bool IsPropertyExist(dynamic settings, string name)
+        {
+            if (settings is ExpandoObject)
+                return ((IDictionary<string, object>)settings).ContainsKey(name);
+
+            return settings.GetType().GetProperty(name) != null;
+        }
+
+        private void readConfig(string ConfigPath, string JsonResult, Config configObj)
+        {
+            Console.WriteLine("read config file !");
+            var tr = new StreamReader(ConfigPath);
+            JsonResult = tr.ReadToEnd();
+            configObj = JsonConvert.DeserializeObject<Config>(JsonResult);
+            tr.Close();
+        }
+
+        private void writeConfig(string ConfigPath, string JsonResult)
+        {
+            using (var tw = new StreamWriter(ConfigPath, true))
+            {
+                tw.WriteLine(JsonResult.ToString());
+                tw.Close();
             }
         }
     }
---
+++ Chip8CSharp/Keyboard.cs
@@ -0,0 +1,65 @@
+﻿using SDL2;
+using System;
+using System.Diagnostics;
+
+namespace Chip8CSharp
+{
+    class Keyboard
+    {
+        public enum Keys
+        {
+            one = 1,
+            two = 2,
+            three = 3,
+            four = 4,
+            five = 5,
+            six = 6,
+            seven = 7,
+            eight = 8,
+            nine = 9,
+            zero = 0,
+            A = 10,
+            B = 11,
+            C = 12,
+            D = 13,
+            E = 14,
+            F = 15
+        }
+        public int KeyCodeToKeyIndex(int keycode)
+        {
+            int keyIndex = 0;
+            if (keycode < 58)
+                keyIndex = keycode - 48;
+            else
+                keyIndex = keycode - 87;
+
+            return keyIndex;
+        }
+
+        public void SDLEventLoop(CPU cpu, Stopwatch frameTimer, bool running)
+        {
+            SDL.SDL_Event sdlEvent;
+            while (SDL.SDL_PollEvent(out sdlEvent) != 0)
+            {
+                if (sdlEvent.type == SDL.SDL_EventType.SDL_QUIT)
+                {
+                    running = false;
+                }
+                else if (sdlEvent.type == SDL.SDL_EventType.SDL_KEYDOWN)
+                {
+                    var key = KeyCodeToKeyIndex((int)sdlEvent.key.keysym.sym);
+                    Console.WriteLine(key);
+                    cpu.Keyboard |= (ushort)(1 << key);
+
+                    if (cpu.WaitingForKeyPress)
+                        cpu.KeyPressed((byte)key);
+                }
+                else if (sdlEvent.type == SDL.SDL_EventType.SDL_KEYUP)
+                {
+                    var key = KeyCodeToKeyIndex((int)sdlEvent.key.keysym.sym);
+                    cpu.Keyboard &= (ushort)~(1 << key);
+                }
+            }
+        }
+    }
+}
